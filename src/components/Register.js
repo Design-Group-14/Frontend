0365bf5b5b8118fb3ea60e31e3ae3e443f68967a
Sultan Albogachiev <Sultanalbo06@gmail.com>
2025-02-20 17:22:38 +0000

    account registration with firebase + removed nodemodules from git

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Register.css';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
 
 const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         if (password !== confirmPassword) {
             alert('Passwords do not match');
             return;
+        }
+        try{
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+            console.log(userCredential)
+            const user = userCredential.user
+            localStorage.setItem('token', user.accessToken)
+            localStorage.setItem('user', JSON.stringify(user))
+            console.log("REGISTERED")
+        }catch(error){
+            console.error(error)
         }
         console.log('Registering with:', email, password);
     };
