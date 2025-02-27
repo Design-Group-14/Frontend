4153a08d32d20e5de06a45208f9c8f999e474492
Sultan Albogachiev <Sultanalbo06@gmail.com>
2025-02-27 03:55:24 +0000

    send request on dashboard start

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Dashboard from '../components/Dashboard';
 
 const DashboardPage = () => {
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const req = new Request("http://127.0.0.1:8000/api/posts/", {
+                    method: "GET",
+                    headers: new Headers({
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                    }),
+                });
+                const response = await fetch(req)
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                console.log(data)
+            } catch (err) {
+                console.log(err.message);
+            } finally {
+                console.log("request finished");
+            }
+        };
+        fetchPosts()
+    }, [])
+
     return (
         <div>
             <Dashboard />
